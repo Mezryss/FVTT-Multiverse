e0840e1d16ca219f454385cc03a43f9999dafb33
Mezryss <mezryss@minalien.dev>
2023-09-02 15:33:23 -0700

    CSS source maps

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
+const config = ctx => ({
+	...ctx.options,
 	plugins: [
 		require('autoprefixer'),
 		require('postcss-import'),
 		require('postcss-nested'),
+		require('cssnano'),
 	],
-};
+});
 
 module.exports = config;
